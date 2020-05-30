085f2934e902b091e3533e3d76901c9b3d061d94
jerry73204 <jerry73204@gmail.com>
2020-05-30 22:28:35 +0800

    Avoid compile error when running cargo test

--- examples/tfrecord_info.rs
+++ examples/tfrecord_info.rs
@@ -1,7 +1,26 @@
-mod common;
-
-use common::INPUT_TFRECORD_PATH;
+use std::{fs::File, io::BufWriter, path::PathBuf};
 use tfrecord::{EasyExampleReader, EasyFeature, Error, RecordReaderInit};
+
+lazy_static::lazy_static! {
+    pub static ref INPUT_TFRECORD_PATH: PathBuf = {
+        let url = "https://storage.googleapis.com/download.tensorflow.org/data/fsns-20160927/testdata/fsns-00000-of-00001";
+        let file_name = "input.tfrecord";
+
+        let data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test_data");
+        std::fs::create_dir_all(&data_dir).unwrap();
+
+        let out_path = data_dir.join(file_name);
+        let mut out_file = BufWriter::new(File::create(&out_path).unwrap());
+        reqwest::blocking::get(url).unwrap().copy_to(&mut out_file).unwrap();
+
+        out_path
+    };
+    pub static ref DATA_DIR: PathBuf = {
+        let data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test_data");
+        std::fs::create_dir_all(&data_dir).unwrap();
+        data_dir
+    };
+}
 
 fn main() -> Result<(), Error> {
     // use init pattern to construct the tfrecord reader
