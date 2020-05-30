cd89ac8413cc3d10a9919b488ceb025e3e7bd4cc
jerry73204 <jerry73204@gmail.com>
2020-05-30 20:08:24 +0800

    Re-export types and traits to root module

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,11 @@ pub mod record;
 mod utils;
 pub mod writer;
 
-pub use protos::Example;
+pub use error::Error;
+pub use markers::GenericRecord;
+pub use protos::{feature::Kind, BytesList, Example, Feature, FloatList, Int64List};
+pub use reader::{
+    BytesReader, EasyExampleReader, ExampleReader, RecordReader, RecordReaderInit, RecordStreamInit,
+};
+pub use record::{EasyExample, EasyFeature};
+pub use writer::{BytesWriter, EasyExampleWriter, ExampleWriter, RecordWriter, RecordWriterInit};
