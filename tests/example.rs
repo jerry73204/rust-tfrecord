121913b91196d178c73abe6020e8491d1455d5b3
jerry73204 <jerry73204@gmail.com>
2020-05-30 20:08:59 +0800

    Test EasyExample type {,de}serialization

--- tests/example.rs
+++ tests/example.rs
@@ -2,9 +2,8 @@ use failure::Fallible;
 use futures::stream::TryStreamExt;
 use std::path::PathBuf;
 use tffile::{
-    reader::{BytesReader, ExampleReader, RecordReaderInit, RecordStreamInit},
-    writer::{BytesWriter, ExampleWriter, RecordWriterInit},
-    Example,
+    BytesReader, BytesWriter, EasyExample, EasyExampleReader, EasyExampleWriter, Example,
+    ExampleReader, ExampleWriter, RecordReaderInit, RecordStreamInit, RecordWriterInit,
 };
 
 lazy_static::lazy_static! {
@@ -32,6 +31,7 @@ lazy_static::lazy_static! {
 
 #[test]
 fn blocking_reader_test() -> Fallible<()> {
+    // bytes
     {
         let reader: BytesReader<_> = RecordReaderInit {
             check_integrity: true,
@@ -40,6 +40,7 @@ fn blocking_reader_test() -> Fallible<()> {
         reader.collect::<Result<Vec<Vec<u8>>, _>>()?;
     }
 
+    // raw examples
     {
         let reader: ExampleReader<_> = RecordReaderInit {
             check_integrity: true,
@@ -48,11 +49,21 @@ fn blocking_reader_test() -> Fallible<()> {
         reader.collect::<Result<Vec<Example>, _>>()?;
     }
 
+    // easy examples
+    {
+        let reader: EasyExampleReader<_> = RecordReaderInit {
+            check_integrity: true,
+        }
+        .open(&*INPUT_TFRECORD_PATH)?;
+        reader.collect::<Result<Vec<EasyExample>, _>>()?;
+    }
+
     Ok(())
 }
 
 #[async_std::test]
 async fn async_stream_test() -> Fallible<()> {
+    // bytes
     {
         let stream = RecordStreamInit {
             check_integrity: true,
@@ -62,6 +73,7 @@ async fn async_stream_test() -> Fallible<()> {
         stream.try_collect::<Vec<Vec<u8>>>().await?;
     }
 
+    // raw examples
     {
         let stream = RecordStreamInit {
             check_integrity: true,
@@ -71,6 +83,16 @@ async fn async_stream_test() -> Fallible<()> {
         stream.try_collect::<Vec<Example>>().await?;
     }
 
+    // easy examples
+    {
+        let stream = RecordStreamInit {
+            check_integrity: true,
+        }
+        .easy_examples_open(&*INPUT_TFRECORD_PATH)
+        .await?;
+        stream.try_collect::<Vec<EasyExample>>().await?;
+    }
+
     Ok(())
 }
 
@@ -78,6 +100,7 @@ async fn async_stream_test() -> Fallible<()> {
 fn blocking_writer_test() -> Fallible<()> {
     let output_path = DATA_DIR.join("blocking_writer_output.tfrecord");
 
+    // bytes
     {
         let reader: BytesReader<_> = RecordReaderInit {
             check_integrity: true,
@@ -93,12 +116,29 @@ fn blocking_writer_test() -> Fallible<()> {
         std::fs::remove_file(&output_path)?;
     }
 
+    // raw examples
     {
         let reader: ExampleReader<_> = RecordReaderInit {
             check_integrity: true,
         }
         .open(&*INPUT_TFRECORD_PATH)?;
         let mut writer: ExampleWriter<_> = RecordWriterInit::create(&output_path)?;
+
+        for result in reader {
+            let example = result?;
+            writer.send(example)?;
+        }
+
+        std::fs::remove_file(&output_path)?;
+    }
+
+    // easy examples
+    {
+        let reader: EasyExampleReader<_> = RecordReaderInit {
+            check_integrity: true,
+        }
+        .open(&*INPUT_TFRECORD_PATH)?;
+        let mut writer: EasyExampleWriter<_> = RecordWriterInit::create(&output_path)?;
 
         for result in reader {
             let example = result?;
@@ -115,6 +155,7 @@ fn blocking_writer_test() -> Fallible<()> {
 async fn async_writer_test() -> Fallible<()> {
     let output_path = DATA_DIR.join("async_writer_output.tfrecord");
 
+    // bytes
     {
         let stream = RecordStreamInit {
             check_integrity: true,
@@ -135,6 +176,7 @@ async fn async_writer_test() -> Fallible<()> {
         async_std::fs::remove_file(&output_path).await?;
     }
 
+    // raw examples
     {
         let stream = RecordStreamInit {
             check_integrity: true,
@@ -142,6 +184,27 @@ async fn async_writer_test() -> Fallible<()> {
         .examples_open(&*INPUT_TFRECORD_PATH)
         .await?;
         let writer: ExampleWriter<_> = RecordWriterInit::create_async(&output_path).await?;
+
+        stream
+            .try_fold(writer, |mut writer, example| {
+                async {
+                    writer.send_async(example).await?;
+                    Ok(writer)
+                }
+            })
+            .await?;
+
+        async_std::fs::remove_file(&output_path).await?;
+    }
+
+    // easy examples
+    {
+        let stream = RecordStreamInit {
+            check_integrity: true,
+        }
+        .easy_examples_open(&*INPUT_TFRECORD_PATH)
+        .await?;
+        let writer: EasyExampleWriter<_> = RecordWriterInit::create_async(&output_path).await?;
 
         stream
             .try_fold(writer, |mut writer, example| {
