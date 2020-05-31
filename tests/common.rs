732b396d3c7587e9abdb9e84eb369f611166e557
jerry73204 <jerry73204@gmail.com>
2020-05-31 22:13:38 +0800

    dataset tests

---
+++ tests/common.rs
@@ -0,0 +1,35 @@
+pub use failure::{ensure, format_err, Fallible};
+#[cfg(feature = "async_")]
+pub use futures::stream::TryStreamExt;
+#[cfg(feature = "serde")]
+pub use prost::Message;
+pub use rand::rngs::OsRng;
+pub use std::{fs::File, io::BufWriter, num::NonZeroUsize, path::PathBuf};
+pub use tfrecord::{
+    BytesReader, BytesWriter, Example, ExampleReader, ExampleWriter, Feature, RawExample,
+    RawExampleReader, RawExampleWriter, RecordReaderInit, RecordWriterInit,
+};
+#[cfg(feature = "async_")]
+pub use tfrecord::{Dataset, DatasetInit, RecordStreamInit};
+
+lazy_static::lazy_static! {
+    pub static ref INPUT_TFRECORD_PATH: PathBuf = {
+
+        let url = "https://storage.googleapis.com/download.tensorflow.org/data/fsns-20160927/testdata/fsns-00000-of-00001";
+        let file_name = "input.tfrecord";
+
+        let data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test_data");
+        std::fs::create_dir_all(&data_dir).unwrap();
+
+        let out_path = data_dir.join(file_name);
+        let mut out_file = BufWriter::new(File::create(&out_path).unwrap());
+        reqwest::blocking::get(url).unwrap().copy_to(&mut out_file).unwrap();
+
+        out_path
+    };
+    pub static ref DATA_DIR: PathBuf = {
+        let data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test_data");
+        std::fs::create_dir_all(&data_dir).unwrap();
+        data_dir
+    };
+}
--- tests/example.rs
+++ tests/example.rs
@@ -1,37 +1,6 @@
-use failure::Fallible;
-#[cfg(feature = "async_")]
-use futures::stream::TryStreamExt;
-#[cfg(feature = "serde")]
-use prost::Message;
-use std::{fs::File, io::BufWriter, path::PathBuf};
-#[cfg(feature = "async_")]
-use tfrecord::RecordStreamInit;
-use tfrecord::{
-    BytesReader, BytesWriter, Example, ExampleReader, ExampleWriter, RawExample, RawExampleReader,
-    RawExampleWriter, RecordReaderInit, RecordWriterInit,
-};
+mod common;
 
-lazy_static::lazy_static! {
-    static ref INPUT_TFRECORD_PATH: PathBuf = {
-
-        let url = "https://storage.googleapis.com/download.tensorflow.org/data/fsns-20160927/testdata/fsns-00000-of-00001";
-        let file_name = "input.tfrecord";
-
-        let data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test_data");
-        std::fs::create_dir_all(&data_dir).unwrap();
-
-        let out_path = data_dir.join(file_name);
-        let mut out_file = BufWriter::new(File::create(&out_path).unwrap());
-        reqwest::blocking::get(url).unwrap().copy_to(&mut out_file).unwrap();
-
-        out_path
-    };
-    static ref DATA_DIR: PathBuf = {
-        let data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test_data");
-        std::fs::create_dir_all(&data_dir).unwrap();
-        data_dir
-    };
-}
+use common::*;
 
 #[test]
 fn blocking_reader_test() -> Fallible<()> {
