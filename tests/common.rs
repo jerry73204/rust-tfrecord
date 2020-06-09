390a80f9a929b3112d181d3530ebd7bf232d5c3d
jerry73204 <jerry73204@gmail.com>
2020-06-09 18:36:41 +0800

    Fix test imports

--- tests/common.rs
+++ tests/common.rs
@@ -5,12 +5,16 @@ pub use futures::stream::TryStreamExt;
 pub use prost::Message;
 pub use rand::rngs::OsRng;
 pub use std::{fs::File, io::BufWriter, num::NonZeroUsize, path::PathBuf};
+#[cfg(feature = "summary")]
+pub use tfrecord::EventWriterInit;
+#[cfg(feature = "async_")]
+pub use tfrecord::RecordStreamInit;
 pub use tfrecord::{
     BytesReader, BytesWriter, Example, ExampleReader, ExampleWriter, Feature, RawExample,
     RawExampleReader, RawExampleWriter, RecordReaderInit, RecordWriterInit,
 };
-#[cfg(feature = "async_")]
-pub use tfrecord::{Dataset, DatasetInit, RecordStreamInit};
+#[cfg(feature = "dataset")]
+pub use tfrecord::{Dataset, DatasetInit};
 
 lazy_static::lazy_static! {
     pub static ref INPUT_TFRECORD_PATH: PathBuf = {
