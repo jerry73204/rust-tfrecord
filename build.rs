ccbef974a74fccc2cb541a4a94fa7fd5aff75b69
jerry73204 <jerry73204@gmail.com>
2021-08-09 08:20:55 +0800

    Refactor build script

--- build.rs
+++ build.rs
@@ -1,32 +1,34 @@
-use anyhow::{bail, Context, Error, Result};
+use anyhow::{bail, format_err, Context, Error, Result};
 use flate2::read::GzDecoder;
 use std::{
     env::{self, VarError},
-    fs::File,
+    fs::{self, File},
     io::{self, BufReader, BufWriter},
     path::{Path, PathBuf},
-    str::FromStr,
 };
 use tar::Archive;
 
-const DEFAULT_TENSORFLOW_VERSION: &str = "2.3.1";
+const TENSORFLOW_VERSION: &str =
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/tensorflow_version"));
+const DEFAULT_TENSORFLOW_URL: &str = concat!(
+    "https://github.com/tensorflow/tensorflow/archive/v",
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/tensorflow_version")),
+    ".tar.gz"
+);
 const BUILD_METHOD_ENV: &str = "TFRECORD_BUILD_METHOD";
 
 lazy_static::lazy_static! {
-    static ref DEFAULT_TENSORFLOW_URL: String = format!("https://github.com/tensorflow/tensorflow/archive/v{}.tar.gz", DEFAULT_TENSORFLOW_VERSION);
-    static ref OUT_DIR: PathBuf = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    static ref CARGO_MANIFEST_DIR: PathBuf = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    static ref OUT_DIR: PathBuf = {
+        PathBuf::from(env::var("OUT_DIR").unwrap())
+    };
     static ref GENERATED_PROTOBUF_FILE: PathBuf = {
-        let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        out_dir.join("tensorflow.rs")
+        OUT_DIR.join("tensorflow.rs")
     };
-    static ref PREBUILD_PROTOBUF_SRC_WITH_SERDE: PathBuf = {
-        let cargo_manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-        cargo_manifest_dir.join("prebuild_src").join("tensorflow_with_serde.rs")
+    static ref PROTOBUF_SRC_W_SERDE: PathBuf = {
+        Path::new(env!("CARGO_MANIFEST_DIR")).join("prebuild_src").join("tensorflow_with_serde.rs")
     };
-    static ref PREBUILD_PROTOBUF_SRC_WITHOUT_SERDE: PathBuf = {
-        let cargo_manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-        cargo_manifest_dir.join("prebuild_src").join("tensorflow_without_serde.rs")
+    static ref PROTOBUF_FILE_WO_SERDE: PathBuf = {
+        Path::new(env!("CARGO_MANIFEST_DIR")).join("prebuild_src").join("tensorflow_without_serde.rs")
     };
 }
 
@@ -36,73 +38,62 @@ enum BuildMethod {
     SrcDir(PathBuf),
     SrcFile(PathBuf),
     InstallPrefix(PathBuf),
-    PreBuild,
-}
-
-impl FromStr for BuildMethod {
-    type Err = Error;
-
-    fn from_str(text: &str) -> Result<Self> {
-        const URL_PREFIX: &str = "url://";
-        const SRC_DIR_PREFIX: &str = "src_dir://";
-        const SRC_FILE_PREFIX: &str = "src_file://";
-        const INSTALL_PREFIX_PREFIX: &str = "install_prefix://";
-        const PREBUILD_PREFIX: &str = "prebuild://";
-
-        let method = if text == PREBUILD_PREFIX {
-            BuildMethod::PreBuild
-        } else if text.starts_with(URL_PREFIX) {
-            let url = text[URL_PREFIX.len()..].to_owned();
-            match url.as_str() {
-                "" => Self::Url(DEFAULT_TENSORFLOW_URL.to_string()),
-                _ => Self::Url(url),
-            }
-        } else if text.starts_with(SRC_DIR_PREFIX) {
-            let dir = PathBuf::from(&text[SRC_DIR_PREFIX.len()..]);
-            Self::SrcDir(dir)
-        } else if text.starts_with(SRC_FILE_PREFIX) {
-            let path = PathBuf::from(&text[SRC_FILE_PREFIX.len()..]);
-            Self::SrcFile(path)
-        } else if text.starts_with(INSTALL_PREFIX_PREFIX) {
-            let prefix = PathBuf::from(&text[INSTALL_PREFIX_PREFIX.len()..]);
-            Self::InstallPrefix(prefix)
-        } else {
-            bail!(r#"invalid build method specification "{}""#, text);
-        };
-
-        Ok(method)
-    }
 }
 
 fn main() -> Result<()> {
     // re-run conditions
     println!(
         "cargo:rerun-if-changed={}",
-        PREBUILD_PROTOBUF_SRC_WITHOUT_SERDE.display()
+        PROTOBUF_FILE_WO_SERDE.display()
     );
-    println!(
-        "cargo:rerun-if-changed={}",
-        PREBUILD_PROTOBUF_SRC_WITH_SERDE.display()
-    );
+    println!("cargo:rerun-if-changed={}", PROTOBUF_SRC_W_SERDE.display());
     println!("cargo:rerun-if-env-changed={}", BUILD_METHOD_ENV);
 
     let build_method = guess_build_method()?;
 
     match build_method {
-        BuildMethod::PreBuild => copy_prebuild_src()?,
-        BuildMethod::Url(url) => build_by_url(&url)?,
-        BuildMethod::SrcDir(dir) => build_by_src_dir(dir)?,
-        BuildMethod::SrcFile(file) => build_by_src_file(file)?,
-        BuildMethod::InstallPrefix(prefix) => build_by_install_prefix(prefix)?,
+        None => {}
+        Some(BuildMethod::Url(url)) => build_by_url(&url)?,
+        Some(BuildMethod::SrcDir(dir)) => build_by_src_dir(dir)?,
+        Some(BuildMethod::SrcFile(file)) => build_by_src_file(file)?,
+        Some(BuildMethod::InstallPrefix(prefix)) => build_by_install_prefix(prefix)?,
     }
 
     Ok(())
 }
 
-fn guess_build_method() -> Result<BuildMethod> {
+#[cfg(not(feature = "generate_protobuf_src"))]
+fn guess_build_method() -> Result<Option<BuildMethod>> {
+    Ok(None)
+}
+
+#[cfg(feature = "generate_protobuf_src")]
+fn guess_build_method() -> Result<Option<BuildMethod>> {
     let build_method = match env::var(BUILD_METHOD_ENV) {
-        Ok(hint) => BuildMethod::from_str(&hint)?,
-        Err(VarError::NotPresent) => BuildMethod::PreBuild,
+        Ok(text) => {
+            const URL_PREFIX: &str = "url://";
+            const SRC_DIR_PREFIX: &str = "src_dir://";
+            const SRC_FILE_PREFIX: &str = "src_file://";
+            const INSTALL_PREFIX_PREFIX: &str = "install_prefix://";
+
+            let method = if let Some(url) = text.strip_prefix(URL_PREFIX) {
+                match url {
+                    "" => BuildMethod::Url(DEFAULT_TENSORFLOW_URL.to_string()),
+                    _ => BuildMethod::Url(url.to_string()),
+                }
+            } else if let Some(dir) = text.strip_prefix(SRC_DIR_PREFIX) {
+                BuildMethod::SrcDir(dir.into())
+            } else if let Some(path) = text.strip_prefix(SRC_FILE_PREFIX) {
+                BuildMethod::SrcFile(path.into())
+            } else if let Some(prefix) = text.strip_prefix(INSTALL_PREFIX_PREFIX) {
+                BuildMethod::InstallPrefix(prefix.into())
+            } else {
+                return Err(build_method_error());
+            };
+
+            method
+        }
+        Err(VarError::NotPresent) => return Err(build_method_error()),
         Err(VarError::NotUnicode(_)) => {
             bail!(
                 r#"the value of environment variable "{}" is not Unicode"#,
@@ -110,7 +101,32 @@ fn guess_build_method() -> Result<BuildMethod> {
             );
         }
     };
-    Ok(build_method)
+    Ok(Some(build_method))
+}
+
+fn build_method_error() -> Error {
+    format_err!(
+        r#"By enabling the "generate_protobuf_src" feature,
+the environment variable "{}" must be set with the following format.
+
+- "url://"
+  Download the source from default URL "{}".
+
+- "url://https://github.com/tensorflow/tensorflow/archive/vX.Y.Z.tar.gz"
+  Download the source from specified URL.
+
+- "src_dir:///path/to/tensorflow/dir"
+  Specify unpacked TensorFlow source directory.
+
+- "src_dir:///path/to/tensorflow/file.tar.gz"
+  Specify TensorFlow source package file.
+
+- "install_prefix:///path/to/tensorflow/prefix"
+  Specify the installed TensorFlow by install prefix.
+"#,
+        BUILD_METHOD_ENV,
+        &*DEFAULT_TENSORFLOW_URL
+    )
 }
 
 fn build_by_url(url: &str) -> Result<()> {
@@ -121,10 +137,7 @@ fn build_by_url(url: &str) -> Result<()> {
     Ok(())
 }
 
-fn build_by_src_dir<P>(src_dir: P) -> Result<()>
-where
-    P: AsRef<Path>,
-{
+fn build_by_src_dir(src_dir: impl AsRef<Path>) -> Result<()> {
     let src_dir = src_dir.as_ref();
 
     // re-run if the dir changes
@@ -134,10 +147,7 @@ where
     Ok(())
 }
 
-fn build_by_src_file<P>(src_file: P) -> Result<()>
-where
-    P: AsRef<Path>,
-{
+fn build_by_src_file(src_file: impl AsRef<Path>) -> Result<()> {
     let src_file = src_file.as_ref();
 
     // re-run if the dir changes
@@ -148,53 +158,20 @@ where
     Ok(())
 }
 
-fn build_by_install_prefix<P>(prefix: P) -> Result<()>
-where
-    P: AsRef<Path>,
-{
+fn build_by_install_prefix(prefix: impl AsRef<Path>) -> Result<()> {
     compile_protobuf(prefix.as_ref().join("include").join("tensorflow"))?;
     Ok(())
 }
 
-fn copy_prebuild_src() -> Result<()> {
-    // check if conflicting "generate_protobuf_src" feature presents
-    if cfg!(feature = "generate_protobuf_src") {
-        bail!(
-            r#"please specify the environment variable "{}" in combination with "generate_protobuf_src" feature"#,
-            BUILD_METHOD_ENV
-        );
-    }
-
-    // copy file
-    let prebuild_file: &Path = if cfg!(feature = "with-serde") {
-        &*PREBUILD_PROTOBUF_SRC_WITH_SERDE
-    } else {
-        &*PREBUILD_PROTOBUF_SRC_WITHOUT_SERDE
-    };
-
-    if !prebuild_file.is_file() {
-        bail!(
-            "Look like the protobuf code is not generated yet. Please read README for instructions"
-        );
-    }
-
-    std::fs::copy(prebuild_file, &*GENERATED_PROTOBUF_FILE)?;
-
-    Ok(())
-}
-
-fn extract_src_file<P>(src_file: P) -> Result<PathBuf>
-where
-    P: AsRef<Path>,
-{
+fn extract_src_file(src_file: impl AsRef<Path>) -> Result<PathBuf> {
     let working_dir = OUT_DIR.join("tensorflow");
     let src_file = src_file.as_ref();
-    let src_dirname = format!("tensorflow-{}", DEFAULT_TENSORFLOW_VERSION);
+    let src_dirname = format!("tensorflow-{}", TENSORFLOW_VERSION);
     let src_dir = working_dir.join(&src_dirname);
 
     // remove previously extracted dir
     if src_dir.is_dir() {
-        std::fs::remove_dir_all(&src_dir)?;
+        fs::remove_dir_all(&src_dir)?;
     }
 
     // extract package
@@ -215,10 +192,7 @@ where
     Ok(src_dir)
 }
 
-fn compile_protobuf<P>(dir: P) -> Result<()>
-where
-    P: AsRef<Path>,
-{
+fn compile_protobuf(dir: impl AsRef<Path>) -> Result<()> {
     let dir = dir.as_ref();
     let include_dir = dir;
     let proto_paths = {
@@ -251,36 +225,20 @@ where
             .collect::<Result<Vec<_>, _>>()?
     };
 
-    // compile protobuf
+    // without serde
     {
-        let mut config = prost_build::Config::new();
-
-        // conditionally use serde
-        if cfg!(feature = "with-serde") {
-            config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
-        }
-
-        config.compile_protos(&proto_paths, &[PathBuf::from(include_dir)])?;
-
-        // verify build
-        if !GENERATED_PROTOBUF_FILE.is_file() {
-            bail!(
-                r#"expect a compiled protobuf code at "{}" but not found"#,
-                GENERATED_PROTOBUF_FILE.display()
-            );
-        }
+        prost_build::compile_protos(&proto_paths, &[PathBuf::from(include_dir)])?;
+        fs::create_dir_all(PROTOBUF_FILE_WO_SERDE.parent().unwrap())?;
+        fs::copy(&*GENERATED_PROTOBUF_FILE, &*PROTOBUF_FILE_WO_SERDE)?;
     }
 
-    // save compiled protobuf code if "generate_protobuf_src" feature presents
-    if cfg!(feature = "generate_protobuf_src") {
-        let prebuild_dest: &Path = if cfg!(feature = "with-serde") {
-            &*PREBUILD_PROTOBUF_SRC_WITH_SERDE
-        } else {
-            &*PREBUILD_PROTOBUF_SRC_WITHOUT_SERDE
-        };
-
-        std::fs::create_dir_all(prebuild_dest.parent().unwrap())?;
-        std::fs::copy(&*GENERATED_PROTOBUF_FILE, prebuild_dest)?;
+    // with serde
+    {
+        prost_build::Config::new()
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .compile_protos(&proto_paths, &[PathBuf::from(include_dir)])?;
+        fs::create_dir_all(PROTOBUF_SRC_W_SERDE.parent().unwrap())?;
+        fs::copy(&*GENERATED_PROTOBUF_FILE, &*PROTOBUF_SRC_W_SERDE)?;
     }
 
     Ok(())
@@ -288,10 +246,10 @@ where
 
 fn download_tensorflow(url: &str) -> Result<PathBuf> {
     let working_dir = OUT_DIR.join("tensorflow");
-    let tar_path = working_dir.join(format!("v{}.tar.gz", DEFAULT_TENSORFLOW_VERSION));
+    let tar_path = working_dir.join(format!("v{}.tar.gz", TENSORFLOW_VERSION));
 
     // createw working dir
-    std::fs::create_dir_all(&working_dir)?;
+    fs::create_dir_all(&working_dir)?;
 
     // return if downloaded package exists
     if tar_path.is_file() {
